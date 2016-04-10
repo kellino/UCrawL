8d18a23f5783dc9f46c702afd03a2d8e1698bc7e
David Kelly <thedavidkelly@hotmail.com>
2016-04-10 12:28:03 +0100

    limit still not working

--- src/UCrawL.py
+++ src/UCrawL.py
@@ -14,23 +14,32 @@ def run(threads, seed, limit):
     """ UCrawL: a simple multi-threaded crawler/indexer designed
         with the goal of scraping a single domain """
     scraper = Scraper()
+    # put the seed in the frontier
     scraper.frontier.put(seed)
+    # create threads
     thread_list = []
     for i in range(threads):
         worker = Worker(target=scraper.crawl, args=())
         worker.setDaemon(True)
         thread_list.append(worker)
         worker.start()
-
+    # check for keyboard interrupt
     while len(thread_list) > 0:
         try:
-            thread_list = [worker.join(threads)
-                           for i in thread_list if worker is not None]
+            # listen for keyboard interrupt
+            pass
         except KeyboardInterrupt:
+            # clean up threads
+            for t in thread_list:
+                if t is not None:
+                    t.join(0.0)
             print "Keyboard interrupt received"
+            # kill the threads
             for worker in thread_list:
                 worker.kill_ordered = True
+            # close the frontier
             scraper.frontier.join()
+            # close
             exit()
 
 if __name__ == '__main__':

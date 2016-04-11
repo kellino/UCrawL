9db340f0d3e8584caa26d4cc3d341ebc344ef57b
David Kelly <thedavidkelly@hotmail.com>
2016-04-11 11:32:20 +0100

    works, but very slow

--- src/indexer.py
+++ src/indexer.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python2.7
-from nltk.tokenize import word_tokenize
+import string
 from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 
 class Indexer():
-    def __init__(self):
+    def __init__(self, rem_punc=True, rem_stop=True):
+        self.rem_punc = rem_punc
+        self.rem_stop = rem_stop
         self.stoplist = stopwords.words('english')
+        self.punctunation = list(string.punctuation)
         self.token_dict = dict()
         self.pst = PorterStemmer()
         self.postings_list = dict()
 
     def get_pages(self):
-        with open('ucl', 'r') as ifile:
+        with open('./data/ucl', 'r') as ifile:
             contents = ifile.read()
             for page in contents.split('visited:'):
                 self.parse_page(page)
 
     def parse_page(self, page):
         page = unicode(page, errors='ignore')
-        tokens = [word for word in word_tokenize(page)
-                  if word not in self.stoplist]
-        tokens = [self.pst.stem(word) for word in tokens]
-        self.add_to_token_dict(tokens[3:])
+        lines = page.strip().split()
+        if len(lines) > 2:
+            title = lines[1]
+            # tokenize and make lowercase
+            tokens = [word.lower() for word in word_tokenize(str(lines[2:]))]
+            # remove punctuation
+            if self.rem_punc:
+                tokens = [word for word in tokens if word not in self.punctunation]
+            # remove stopwords
+            if self.rem_stop:
+                tokens = [word for word in tokens if word not in self.stoplist]
+            # stem (Porter stemmer)
+            tokens = [self.pst.stem(word) for word in tokens]
+            # add to dictionary
+            self.add_to_token_dict(title, tokens[3:])
 
-    def add_to_token_dict(self, tokens):
+    def add_to_token_dict(self, title, tokens):
         if tokens:
-            title = tokens[0]
             words = dict()
             for token in tokens[1:]:
                 key = self.pst.stem(token.lower())
@@ -44,11 +58,11 @@ class Indexer():
 if __name__ == '__main__':
     indexer = Indexer()
     indexer.get_pages()
-    with open('complete_dictionary', 'a') as cd:
-        for k, v in indexer.token_dict.iteritems():
-            cd.writelines("word {}, frequency {}\n".format(k, v))
     for k, v in indexer.postings_list.iteritems():
         print k,
         for (x, y) in v:
             print "{}:{}".format(x, y),
         print "\n"
+    with open('complete_dictionary', 'w') as cd:
+        for k, v in indexer.token_dict.iteritems():
+            cd.writelines("{} {}\n".format(k, v))

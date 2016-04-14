bc0855fc0bd9f6519d4ddadd09ff391583d5bf3f
David Kelly <thedavidkelly@hotmail.com>
2016-04-14 09:34:14 +0100

    changed logging

--- src/scraper.py
+++ src/scraper.py
@@ -2,20 +2,28 @@
 from __future__ import print_function
 import requests
 from bs4 import BeautifulSoup
+import logging
 import urlparse
 import robotparser
 import re
 import Queue
 
 
+class RobotParseException(Exception):
+    def __init__(self):
+        Exception.__init__(self)
+
+
 class Scraper:
     def __init__(self, robotParserEnabled=True, restrictedDomain=True):
-        self.logger = utils.logger.load_logger()
+        self.logger = logging.getLogger('UCrawL')
+        self.logger.debug('initializing scraper')
         self.robotDict = {}
         self.frontier = Queue.Queue(0)
         self.visited = set()
         self.robotParserEnabled = robotParserEnabled
         self.restrictedDomain = restrictedDomain
+        # list of extensions to ignore
         self.illegal = [".mp4", ".mp3", ".flv", ".m4a",
                         ".jpg", ".png", ".gif",
                         ".xml", ".pdf", ".gz", ".zip", ".rss"]
@@ -23,7 +31,6 @@ class Scraper:
     def visit(self, url):
         """ visits a given url and returns all the data """
         # normalize urls
-        self.logger.info("visiting {}".format(url))
         if (url.startswith("http://") or url.startswith("https://")) is False:
             url = "http://" + url
         domain = urlparse.urlsplit(url)[1].split(':')[0]
@@ -38,7 +45,7 @@ class Scraper:
                     c = r.content
                     soup = BeautifulSoup(c.decode('utf-8', 'ignore'), 'lxml')
                     found_links = self.get_links(soup, domain)
-                    self.get_text(url, soup)
+                    self.save_text(url, soup)
                     r.close()
                 for link in found_links:
                     if link not in self.visited:
@@ -46,9 +53,11 @@ class Scraper:
                             continue
                         else:
                             self.frontier.put(link)
-                # self.print_all_links(url, found_links)
+                self.print_all_links(url, found_links)
+        except RobotParseException:
+            self.logger.info('robots.txt forbids scraping of {}'.format(url))
         except:
-            self.logger.info("robots.txt forbids crawling of {}".format(url))
+            self.logger.warn('unable to connect to server')
 
     def robot_parse(self, httpDomain, url):
         # do not attempt to parse any page which the robots.txt forbids
@@ -59,7 +68,7 @@ class Scraper:
             self.robotDict[httpDomain] = rp
             isParsable = rp.can_fetch("*", url)
             if not isParsable:
-                raise Exception("RobotParse")
+                raise RobotParseException()
 
     def get_links(self, soup, domain):
         # extracts all the (domain) links on a visited page, returning them as
@@ -78,13 +87,13 @@ class Scraper:
         return foundUrls
 
     def print_all_links(self, url, found_links):
-        # dump info to stout
-        print("{} = [".format(url), end="")
-        for link in found_links:
-            print("{}, ".format(link), end="")
-        print("]\n")
+        try:
+            with open('./../data/link_list', 'a') as f:
+                f.write('{} {}\n'.format(url, str(found_links)))
+        except IOError:
+            self.logger.warn('unable to write/append to file')
 
-    def get_text(self, url, soup):
+    def save_text(self, url, soup):
         # thanks to Hugh Bothwell and Bumpkin on StackOverflow for this!
         for script in soup(["script", "style"]):
             script.extract()
@@ -92,15 +101,15 @@ class Scraper:
         lines = (line.strip() for line in text.splitlines())
         chunks = (phrase.strip() for line
                   in lines for phrase in line.split("  "))
-        text = '\n'.join(chunk for chunk in chunks if chunk)
-        print("visited: {}".format(len(self.visited)+1))
-        print(url)
-        print(text.encode('utf-8'))
-        print('\n')
+        text = ' '.join(chunk for chunk in chunks if chunk)
+        try:
+            with open('./../data/ucl_text', 'a') as f:
+                f.write("{} {}\n".format(url, text.encode('utf-8')))
+        except:
+            self.logger.warn('unable to write/append to file')
 
     def crawl(self, stop_event):
         while not stop_event.wait(1):
             url = self.frontier.get()
             self.visit(url)
             self.visited.add(url)
-            # time.sleep(polite)
